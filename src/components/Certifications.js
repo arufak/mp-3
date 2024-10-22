f22a6c6f3f9882dd01a4aafb7eba279e0c065c20
Arufa Khanom <arufamanar1@gmail.com>
2024-10-22 18:39:20 -0400

    updated

--- src/components/Certifications.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export default function Certifications() {
-    return (React.createElement("main", null,
-        React.createElement("section", { className: "title" },
-            React.createElement("h1", null, "No certifications yet - but stay tuned!"))));
-}
-;
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export default function Header() {
-    return (React.createElement("header", null,
-        React.createElement("h5", null, "Arufa Khanom"),
-        React.createElement("h6", null, "Online Resume")));
-}
-;
