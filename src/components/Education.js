0a09da116b42ed2d1144363777d7f1c3ffd81d7f
Arufa Khanom <arufamanar1@gmail.com>
2024-10-22 18:32:33 -0400

    updated mp-3

---
+++ src/components/Education.js
@@ -0,0 +1,27 @@
+import React from 'react';
+export default function Education() {
+    return (React.createElement("main", null,
+        React.createElement("section", { className: "title" },
+            React.createElement("h1", null, "EDUCATION"),
+            React.createElement("h2", null, "Boston University (BU), Boston, MA"),
+            React.createElement("p", null,
+                React.createElement("strong", null, "Degree:"),
+                " Bachelor of Arts in Computer Science"),
+            React.createElement("p", null,
+                React.createElement("strong", null, "Minor:"),
+                " Business Administration and Management"),
+            React.createElement("p", null,
+                React.createElement("strong", null, "Graduation Date:"),
+                " May 2025"),
+            React.createElement("h3", null, "Subjects of Interest:"),
+            React.createElement("ul", null,
+                React.createElement("li", null, "Data Structures and Algorithms"),
+                React.createElement("li", null, "Database Systems"),
+                React.createElement("li", null, "Web Development"),
+                React.createElement("li", null, "Data Analytics"),
+                React.createElement("li", null, "Machine Learning"),
+                React.createElement("li", null, "Business Management"),
+                React.createElement("li", null, "Product Management")),
+            React.createElement("p", null, "During my time at Boston University, I have developed a solid foundation in computer science principles and gained business management skills, which have prepared me for diverse technical roles in the industry. I have been involved in various academic projects and have taken courses that emphasize both technical and business-oriented problem-solving skills."))));
+}
+;
---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Use React Router for navigation
+export default function Nav() {
+    return (React.createElement("nav", null,
+        React.createElement("ul", null,
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/" }, "Home")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/education" }, "Education")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/experiences" }, "Experiences")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/certifications" }, "Certifications")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/projects" }, "Projects")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/achievements" }, "Achievements")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/skills" }, "Skills")),
+            React.createElement("li", null,
+                React.createElement(Link, { to: "/references" }, "References")))));
+}
+;
